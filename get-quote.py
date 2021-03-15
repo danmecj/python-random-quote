dd35c67785c208b91446c589ce9895edc640dbe4
Danial Mecja <danialmecja@gmail.com>
2021-03-15 20:49:54 +0800

    Add new line of random quote

--- get-quote.py
+++ get-quote.py
@@ -8,12 +8,12 @@ def primary():
   f.close()
 
 # Last quote line number in the .txt
-  last = 13
+  last = len(quotes) - 1
 
 # Generate a random choice from 0th line to 13th line
   rand = random.randint(0, last)
 
-  print(quotes[rand])
-
+  print(quotes[rand] + quotes[rand - 3])
+  #print(quotes[rand - 2])
 if __name__== "__main__":
   primary()
